183b6d83f42b5f85f3666aebbcf3d6d62c8f1154
hztraver <62444149+hztraver@users.noreply.github.com>
2020-03-20 12:02:57 -0700

    Update RPDRrandomforests.R

--- RPDRrandomforests.R
+++ RPDRrandomforests.R
@@ -16,22 +16,27 @@ data$Class <- ifelse(data$Outcome == "Winner" | data$Outcome == "Runner-up", "TO
 rf_data <- data[data$Outcome!="TBD",]
 rf_data$Class <- as.factor(rf_data$Class) 
 
-##RANDOM FORESTS## DO NOT RE-RUN ##
+##RANDOM FORESTS#
 features = c('State','Town_Pop','Age','Ethnicity','Type','perc_win','perc_btm','Insta_perc','Class')
 #select traning data
 train_index <- sample(1:nrow(rf_data),80)
 train_data <- rf_data[train_index, features]
-
+#train rf
 statRF <- randomForest(formula = Class ~ ., data = train_data,mtry=5, ntree=400,importance=TRUE)
 statRF
+#Variable importance plot
 varImpPlot(statRF)
-plot(statRF)
-margin(statRF)
 
 #select testing data
 test_data <- rf_data[-train_index, features]
+#prediction classes
 testRF <- predict(statRF, test_data)
+#confusion matrix
 table(observed=test_data[,9],predicted=testRF)
+#prediction class probabilities
+testRF_prob <- predict(statRF, test_data, type="prob")
+#Who was misclassified???
+test_pred <- data.frame(test_data$Contestant, test_data$Class, testRF, testRF_prob)
 
 ##SEASON 12 PREDICTIONS##
 seas12 <- data[data$Outcome=='TBD',]
